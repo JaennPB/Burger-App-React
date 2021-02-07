9644d92867117426bbce81cc8ef7eae0aaac0c75
jaennpb <jaenn.bucio@gmail.com>
2021-02-07 12:50:29 -0600

    commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+  state = {};
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>Burger builder</div>
+        <div>Burger controls</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default BurgerBuilder;
